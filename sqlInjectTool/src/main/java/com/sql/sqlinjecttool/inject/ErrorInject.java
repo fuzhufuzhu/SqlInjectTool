e98d41761f0f4a24f82240b78c6a96031112c9db
zhangJiaLiang <zjl930@126.com>
2022-01-25 16:16:35 +0800

    微调部分代码

---
+++ sqlInjectTool/src/main/java/com/sql/sqlinjecttool/inject/ErrorInject.java
@@ -0,0 +1,65 @@
+package com.sql.sqlinjecttool.inject;
+
+import com.sql.sqlinjecttool.judge.Judge;
+import com.sql.sqlinjecttool.paramOperation.ParamOperation;
+import com.sql.sqlinjecttool.payload.ErrorPayload;
+import com.sql.sqlinjecttool.pojo.UserInput;
+import com.sql.sqlinjecttool.util.ResolvingPost;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+
+
+public class ErrorInject extends Inject {
+
+
+    public ErrorInject(UserInput userInput) {
+        super(userInput);
+        System.out.println("进来了");
+    }
+    public ErrorInject(ResolvingPost resolvingPost){
+        super(resolvingPost);
+        System.out.println("POST");
+    }
+
+    public String aInject() throws IOException {
+
+        ParamOperation paramOperation = new ParamOperation();
+        Judge judge = new Judge();
+       // UserInput errorPayloadInput = null;
+
+        if (isButtonStatus()){
+            return null;
+        }
+        for(int i = 0; i< ErrorPayload.errorPayload.size(); i++){
+            UserInput errorPayloadInput =null;
+            if (1==1){
+                ResolvingPost resolvingPost1 = (ResolvingPost) getResolvingPost().clone();
+                //if (isMarkStatus()){
+                if (1 == 1) {
+
+                    String markValue = resolvingPost1.getMarkValue();
+
+                    String errorPayload = paramOperation.markConnect(markValue,ErrorPayload.errorPayload.get(i).toString());
+                    resolvingPost1.setHashMap(resolvingPost1.getMarkKey(), errorPayload);
+                    errorPayloadInput = new UserInput(resolvingPost1,getMethod());
+                }
+                else {
+//                    String temp = correctInput.getFrontPart();
+//                    System.out.println();
+//                    errorPayloadInput =new UserInput(temp+errorPayload);
+                }
+                int mark= judge.ErrorJudge(errorPayloadInput);
+
+                if(mark==1){
+                    System.out.println(mark);
+                    System.out.println("检测到sql注入，url为"+ URLDecoder.decode(errorPayloadInput.getUrl()));
+                   // button = 1;
+                    break;
+                }
+            }
+        }
+        return "123";
+
+    }
+}
---
+++ sqlInjectTool/src/main/java/com/sql/sqlinjecttool/inject/Inject.java
@@ -0,0 +1,60 @@
+package com.sql.sqlinjecttool.inject;
+
+import com.sql.sqlinjecttool.pojo.UserInput;
+import com.sql.sqlinjecttool.util.ResolvingPost;
+
+public class Inject {
+    private boolean buttonStatus;
+    private boolean markStatus;
+    private UserInput userInput;
+    private String method;
+    private ResolvingPost resolvingPost;
+
+
+
+   public Inject(UserInput userInput)
+   {
+        this.userInput = userInput;
+        setMethod();
+    }
+
+    public Inject(ResolvingPost resolvingPost){
+       setResolvingPost(resolvingPost);
+    }
+
+
+
+
+
+    public UserInput getUserInput(){
+     return userInput;
+    }
+
+    public boolean isButtonStatus(){
+        return buttonStatus;
+    }
+
+    public boolean isMarkStatus() {
+        return markStatus;
+    }
+
+    public void setUserInput(UserInput userInput) {
+        this.userInput = userInput;
+    }
+    public void setMethod() {
+        this.method = userInput.getMethod();
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+     ResolvingPost getResolvingPost() {
+        return resolvingPost;
+    }
+
+    public void setResolvingPost(ResolvingPost resolvingPost) {
+        this.resolvingPost = resolvingPost;
+    }
+
+}
